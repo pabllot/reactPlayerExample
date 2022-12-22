41af3bed3b168bd776209987b1c7570081cc953d
Pabllot07 <pabllot07@outlook.com>
2022-12-21 23:53:51 -0300

    change speed

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,10 @@
-import { useState, useRef, useEffect } from 'react'
-import video from './assets/example.mp4'
-import { IoMdPlay, IoMdPause, MdVolumeOff, BiFullscreen, BiExitFullscreen, CgScreenWide, CgScreen, GoUnmute} from './assets/reactIconsImport';
-import './App.css'
+import React from 'react'
+import Video from './Video'
 
-function App() {
-  const [isVideoPaused, setIsVideoPaused] = useState(true)
-  const [isTheaterMode, setIsTheaterMode] = useState(false)
-  const [isFullScreen, setIsFullScreen] = useState(true)
-  const [isMute, setIsMute] = useState(true)
-  const [duration, setDuration] = useState(null)
-  const [currentTime, setCurrentTime] = useState('0:00')
-
-
-  const vidRef = useRef(null)
-  const fullScreenRef = useRef(null)
-  const volumeRef = useRef(null)
-
-  const toggleTheaterMode = () =>  setIsTheaterMode(prev => !prev);
-
-  const toggleMute = () => {
-    setIsMute(prev => !prev)
-    isMute ? vidRef.current.volume = 0 : vidRef.current.volume = 1;
-    isMute ? volumeRef.current.value = 0 : volumeRef.current.value = 1
-    }
-
-
- const toggleFullScreen = () => {
-    setIsFullScreen(prev => !prev)    
-    if (!isFullScreen)document.exitFullscreen()
-    else fullScreenRef.current.requestFullscreen()
-    }
-
- const togglePlay = () => {
-    isVideoPaused ? vidRef.current.play() : vidRef.current.pause();
-    setIsVideoPaused(prev => !prev)
-    }
-
- // ajustar volume de acordo com a barrinha
- const  handleRange= (e)=>{
-    if(e.target.value < 0.02) vidRef.current.volume = 0, setIsMute(false);
-    else if(e.target.value > 0 && e.target.value < 0.6) vidRef.current.volume = 0.4, setIsMute(true)
-    else vidRef.current.volume = 1
-    }
-
-
-  // Se os segundos começarem com zero exemplo 01:03. Isso ajeita, senao fica 01:3.
-  const startWithZero = new Intl.NumberFormat(undefined, {
-    minimumIntegerDigits: 2,
-    })
-
-  // formatando a duração de vídeo, pq recebe milisegundos e nao em formato de hora 
-  const formatDuration = (time) => {
-    const seconds = Math.floor(time % 60)
-    const minutes = Math.floor(time / 60) % 60
-    const hours = Math.floor(time / 3600)
-    if (hours === 0) {
-      return `${minutes}:${startWithZero.format(seconds)}`
-      } else {
-        return `${hours}:${startWithZero.format(minutes)}:${startWithZero.format(seconds)}`
-      }
-   }
-  
-
+const App = () => {
   return (
-    <div  className="App">
-      <div ref={fullScreenRef} className={`video-container paused  ${isTheaterMode ? "theater" : ""}`}>
-        <div className='video-controls-container'>
-          <div className="timeline-container"></div>
-          <div className="controls">
-            <button onClick={togglePlay}>{!isVideoPaused ? <IoMdPause /> : <IoMdPlay />}</button>
-
-            <div className='volume-container'>
-              <button onClick={toggleMute}>{isMute ? <GoUnmute /> : <MdVolumeOff />}</button>
-              <input ref={volumeRef} onInput={(e) => handleRange(e)} className='volume-slider' type='range' min='0' max='1' step='any' defaultValue='1'></input>
-            </div>
-
-            <div className='duration-container'>
-              <div className='current-time'>{currentTime}</div>/
-              <div className='total-time'>{duration}</div>
-            </div>
-
-            <button onClick={toggleTheaterMode}>{isTheaterMode ? <CgScreenWide/> : <CgScreen />}</button>
-            <button onClick={toggleFullScreen}>{ isFullScreen ? <BiFullscreen /> : <BiExitFullscreen/>}</button>
-          </div>
-        </div>
-        <video ref={vidRef} onTimeUpdate={()=>setCurrentTime(formatDuration(vidRef?.current?.currentTime))} onLoadedMetadata={()=>setDuration(formatDuration(vidRef?.current?.duration))} src={video} loop type='video/mp4'></video>
-      </div>
+    <div>
+        <Video />
     </div>
   )
 }
